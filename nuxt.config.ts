0338c49dc4b24766b0bbed55b52a3d8835b8875b
JaxQian <qianjunyinggo@gmail.com>
2024-02-07 18:08:49 +0800

    feature: 更改构建目录

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,9 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   ssr: false,
   modules: ["@nuxtjs/tailwindcss"],
+  nitro: {
+    output: {
+      publicDir: 'docs'
+    }
+  }
 })
