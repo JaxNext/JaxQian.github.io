8cccdb04adf1caa5fe11bc894351efe50b6311f6
JaxQian <qianjunyinggo@gmail.com>
2024-02-07 19:11:08 +0800

    feature: 调整构建产物文件名格式

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+const INVALID_CHAR_REGEX = /[\x00-\x1F\x7F<>*#"{}|^[\]`;?:&=+$,]/g
+const DRIVE_LETTER_REGEX = /^[a-z]:/i
 export default defineNuxtConfig({
   devtools: { enabled: true },
   ssr: false,
@@ -10,5 +12,22 @@ export default defineNuxtConfig({
   },
   app: {
     buildAssetsDir: '/static/'
+  },
+  vite: {
+    build: {
+      rollupOptions: {
+        output: {
+          // https://github.com/rollup/rollup/blob/master/src/utils/sanitizeFileName.ts
+          sanitizeFileName(name) {
+            const match = DRIVE_LETTER_REGEX.exec(name)
+            const driveLetter = match ? match[0] : ''
+            return (
+              driveLetter +
+              name.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, '')
+            )
+          },
+        }
+      }
+    }
   }
 })
